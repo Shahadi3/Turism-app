62ded7a47d5450b48c83bf56078a030f3daf9123
Shahad <Shahad98_@hotmail.com>
2022-05-31 12:15:19 +0300

    Initial Commit

---
+++ Turism app/Turism_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Turism_appApp.swift
+//  Turism app
+//
+//  Created by Shahad Abdullah on 01/11/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct Turism_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
